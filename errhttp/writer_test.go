1c965a89846bb136601758c8dfb88d1c873714d1
Kuba Siemiatkowski <jakub.siemiatkowski@grafana.com>
2023-02-10 12:14:08 +0100

    - test return value

--- errhttp/writer_test.go
+++ errhttp/writer_test.go
@@ -15,15 +15,16 @@ func TestWrite(t *testing.T) {
 	ctx := context.Background()
 	const msgID = "test.thisIsExpected"
 	base := gerror.New(gerror.StatusTimeout, msgID)
-	handler := func(writer http.ResponseWriter, request *http.Request) {
-		Write(ctx, base.Errorf("got expected error"), writer)
+	handler := func(writer http.ResponseWriter, request *http.Request) error {
+		return Write(ctx, base.Errorf("got expected error"), writer)
 	}
 
 	req := httptest.NewRequest("GET", "http://localhost:3000/fake", nil)
 	recorder := httptest.NewRecorder()
 
-	handler(recorder, req)
+	ret := handler(recorder, req)
 
 	assert.Equal(t, http.StatusGatewayTimeout, recorder.Code)
 	assert.JSONEq(t, `{"message": "Timeout", "messageId": "test.thisIsExpected", "statusCode": 504}`, recorder.Body.String())
+	assert.Nil(t, ret)
 }
